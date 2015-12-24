bc0555ef71a4d5744174993b0c8722bea211bdd5
tungnk1993 <tungnk1993@gmail.com>
2015-12-24 17:25:49 +0700

    Add function to show remaining char in review content box

--- mysite/static/write-review.js
+++ mysite/static/write-review.js
@@ -1,6 +1,6 @@
 $(function () {
   $('[data-toggle="popover"]').popover({
-  	container: 'body'
+	container: 'body'
   });
 
   $('.rating-tooltip').rating({
@@ -12,4 +12,31 @@ $(function () {
 		});
 	  }
 	});
+
+  var text_max = 100;
+  var text_length = $('#review-content').val().length;
+  var text_remaining = text_max - text_length;
+
+  if (text_remaining <= 0) 
+	$('#textarea-feedback').html("Đã đủ số kí tự tối thiểu :)");
+  else
+	$('#textarea-feedback').html("Cố lên, " + (text_max - text_length) + " kí tự nữa thôi!");
+
+	$('#review-content').keyup(function() {
+		var text_length = $('#review-content').val().length;
+		var text_remaining = text_max - text_length;
+
+		if (text_remaining <= 0) 
+		{
+			$('#textarea-feedback').html("Đã đủ số kí tự tối thiểu :)");
+			$('#textarea-feedback').removeClass('text-danger');
+		}
+		else
+		{
+			$('#textarea-feedback').html("Cố lên, " + (text_max - text_length) + " kí tự nữa thôi!");
+			$('#textarea-feedback').addClass('text-danger');
+		}
+	});
+
+			
 })
